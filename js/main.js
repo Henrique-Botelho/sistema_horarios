07f9ba90e472117a42ad10be5d1c1892d934286f
Henrique de Moraes Botelho da Silva <henriquebotelho@usp.br>
2026-02-02 23:58:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import Estagiario from "./classes/Estagiario.js";
 
 
-let estag = new Estagiario("Henrique", "Física", ["7:25", "13:00", "14:00", "15:30"]);
+let estag = new Estagiario("Henrique", "Física", ["7:25", "12:00", "13:00", "15:15"]);
 
 
 console.log(estag.calcula_total_horas() / 60);
