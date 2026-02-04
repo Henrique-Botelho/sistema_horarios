307e96d8733245aae76e8b5fabc2f7d104ea5fd7
Henrique de Moraes Botelho da Silva <henriquebotelho@usp.br>
2026-02-04 15:07:15 -0300

    refatoração da tabela

--- js/classes/Estagiario.js
+++ js/classes/Estagiario.js
@@ -15,8 +15,19 @@ export default class Estagiario {
    */
   constructor(horario) {
     this.#horario = horario;
+  }
 
-    this.#horario.map(item => this.#calcula_total_horas(item));
+  /**
+   * Função para pegar o total de horas do estagiário.
+   * @param {string} formato Formato de retorno do total de horas. Padrão "horas".
+   * @returns Total de horas
+   */
+  get_total_horas(formato = "horas") {
+    if (formato === "horas") {
+      return (this.#total_horas / 60).toFixed(2);
+    } else if (formato === "minutos") {
+      return this.#total_horas.toString();
+    }
   }
 
   /**
@@ -37,34 +48,34 @@ export default class Estagiario {
    * 
    * @returns {number} Número total de tempo do estagiário na escola em minutos.
    */
-  #calcula_total_horas(horario_dia) {
-    let [ manha_inicio, manha_fim, tarde_inicio, tarde_fim ] = horario_dia.map(String);
-    let horarios = HORARIOS.filter(item => this.#calcula_intervalo(manha_inicio,item.FIM) > 0 );
-    let horario_inicial;
-    let horario_final;
-    let contador = 0;
+  calcula_total_horas() {
+    this.#horario.map(item => {
+      let [ manha_inicio, manha_fim, tarde_inicio, tarde_fim ] = item.map(String);
+      let horarios = HORARIOS.filter(item => this.#calcula_intervalo(manha_inicio,item.FIM) > 0 );
+      let horario_inicial;
+      let horario_final;
+      let contador = 0;
 
-    // Parte da manhã
-    for (let x = 0; x < horarios.length; x++) {
-      if (x == 0) {
-        horario_inicial = manha_inicio;
-      } else {
-        horario_inicial = horarios[x].INICIO
+      // Parte da manhã
+      for (let x = 0; x < horarios.length; x++) {
+        if (x == 0) {
+          horario_inicial = manha_inicio;
+        } else {
+          horario_inicial = horarios[x].INICIO
+        }
+        horario_final = horarios[x].FIM;
+
+        if(this.#calcula_intervalo(horario_final, manha_fim) <= 0) {
+          this.#total_horas += this.#calcula_intervalo(horario_inicial, manha_fim);
+          break;
+        }
+
+        this.#total_horas += this.#calcula_intervalo(horario_inicial, horario_final);
       }
-      horario_final = horarios[x].FIM;
 
-      if(this.#calcula_intervalo(horario_final, manha_fim) <= 0) {
-        this.#total_horas += this.#calcula_intervalo(horario_inicial, manha_fim);
-        break;
-      }
-
-      this.#total_horas += this.#calcula_intervalo(horario_inicial, horario_final);
-    }
-
-    // Parte da tarde
-    this.#total_horas += this.#calcula_intervalo(tarde_inicio, tarde_fim);
-
-    // return tempo_total;
+      // Parte da tarde
+      this.#total_horas += this.#calcula_intervalo(tarde_inicio, tarde_fim);
+    });
   }
 
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 import Estagiario from "./classes/Estagiario.js";
-import Horarios from "./classes/Horarios.js";
+import { DIAS, PERIODOS } from "./classes/Dados.js";
 
+// const btn_calcular = document.getElementById("btn_calcular");
+// const container = document.getElementById("container");
+// const resultado = document.getElementById("resultado");
 
-const horario = new Horarios();
+// btn_calcular.addEventListener("click", retorno_dados);
+
+// function retorno_dados() {
+//   let horarios = [];
+//   for (let i = 1; i < container.children.length; i++) {
+//     let dia = [];
+//     for (let j = 1; j < container.children[i].children.length; j++) {
+//       dia.push(container.children[i].children[j].value);
+//     }
+
+//     horarios.push(dia);
+//   }
+
+//   const estagiario = new Estagiario(horarios);
+//   estagiario.calcula_total_horas();
+
+//   resultado.textContent = estagiario.get_total_horas();
+// }
